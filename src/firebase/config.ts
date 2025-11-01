59ee6793607baef1a393dfc5e4f6c8c4aeaace3c
Tamim Iqbal Fysal <tamimiqbal.fysal@gmail.com>
2025-11-01 13:56:33 +0000

    how to import all data from firebase prioject id studio-6980545386-3d0e9

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6980545386-d7bac",
-  "appId": "1:991789938277:web:ee5299485f35291eeab88c",
+  "projectId": "studio-6980545386-3d0e9",
+  "appId": "1:991789938277:web:8d22f0c5f21d3319eab88c",
   "apiKey": "AIzaSyC9cNz3BPu_a83SlZFod2VrQyzTSQBTHHA",
-  "authDomain": "studio-6980545386-d7bac.firebaseapp.com",
+  "authDomain": "studio-6980545386-3d0e9.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "991789938277"
 };
