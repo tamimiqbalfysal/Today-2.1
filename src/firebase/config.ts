e659564efcc171f30e36059e0a7110ebd3fdc564
Tamim Iqbal Fysal <tamimiqbal.fysal@gmail.com>
2025-11-01 04:31:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6980545386-d7bac",
+  "appId": "1:991789938277:web:ee5299485f35291eeab88c",
+  "apiKey": "AIzaSyC9cNz3BPu_a83SlZFod2VrQyzTSQBTHHA",
+  "authDomain": "studio-6980545386-d7bac.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "991789938277"
+};
